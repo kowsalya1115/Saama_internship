1e04f67d62b153c11a73ede2778f067473a9a557
kowsalya1115 <83444582+kowsalya1115@users.noreply.github.com>
2021-06-07 06:02:16 -0400

    Add files via upload

--- tamilword_analysis.py
+++ tamilword_analysis.py
@@ -1,20 +1,44 @@
-file_value=open("sample.txt",'r')
+def letter_frequency(value):
+	b={}
+	for i in value:
+		k=b.keys()
+		if i in k:
+			b[i]=b[i]+1
+		else:
+			b[i]=1
+	for k,v in b.items():
+		print(k,v)
+
+def word_frequency(value):
+	text=value.lower()
+	skips=[".",",",":","'",'"']
+	for i in skips:
+		text=text.replace(i,"")
+	new_text=text.split(" ")
+	word_count={}
+	for i in new_text:
+		if i in word_count:
+			word_count[i]=word_count[i]+1
+		else:
+			word_count[i]=1
+	print(word_count)
+
+def sort_word(value):
+	words=[word.lower()for word in value.split()]
+	words.sort()
+	for word in words:
+		print(word)
+def find_grams(value):
+	n=int(input("Enter the number of grams:"))
+	sentence=value.split()
+	grams=[sentence[i:i+n] for i in range(len(sentence)-n+1)]
+	for gram in grams:
+		print(gram)
+
+file_value=open("tamil_word.txt",'r')
 a=file_value.read()
-print(a)
-my_str="அகர முதல எழுத்தெல்லாம் ஆதி "
-words=[word.lower()for word in my_str.split()]
-words.sort()
-print("the sorted words are:")
-for word in words:
-	print(word)
-sentence="அகர முதல எழுத்தெல்லாம் ஆதி .".split()
-N=2
-grams=[sentence[i:i+N] for i in range(len(sentence)-N+1)]
-for gram in grams:
-	print(gram)
-sentence="அகர முதல எழுத்தெல்லாம் ஆதி.".split()
-N=3
-grams=[sentence[i:i+N]for i in range(len(sentence)-N+1)]
-for gram in grams:
-	print(gram)
-	
+letter_frequency(a)
+word_frequency(a)
+sort_word(a)
+find_grams(a)
+
